d7c3903877669e8b6eabe6f92226217065ada61e
dhruvrangholiya114 <130462930+dhruvrangholiya114@users.noreply.github.com>
2023-04-26 17:23:16 +0530

    Update temperate8.c

--- temperate8.c
+++ temperate8.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+main()
 
-main() 
 {
-
-  int n, i, flag = 0;
-  printf("Enter a positive integer: ");
-  scanf("%d", &n);
-
-  
-  if (n == 0 || n == 1)
-    flag = 1;
-
-  for (i = 2; i <= n / 2; ++i) {
-
-    
-    if (n % i == 0) {
-      flag = 1;
-      break;
-    }
-  }
-
-  
-  if (flag == 0)
-    printf("%d is a prime number.", n);
-  else
-    printf("%d is not a prime number.", n);
-
-  return 0;
+	int n,i,num=0;
+	printf("enter number :");
+	scanf("%d",&n);
+	
+	if (n==0 && n==1)
+	{
+		printf("%d is not prime number",n);
+	}
+	else
+	{
+	for (i=2;i<=n/2;i++)
+	{
+		if (n%i==0)
+		{
+			num=1;
+		}
+	}
+	if (num==0)
+	{
+		printf("%d is prime number",n);
+		
+	}
+	else
+	{
+		printf("%d is not a prime number",n);
+	}
+   }
 }
